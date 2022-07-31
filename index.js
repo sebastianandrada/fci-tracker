2dbbbcf6ccfa1155c5a18511557cb798890e62b8
Sebastian Andrada <andrada.sebastian93@gmail.com>
2022-07-31 18:58:10 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const axios = require("axios"),
+  schedule = require("node-schedule");
+const { Telegraf } = require("telegraf");
+require('dotenv').config();
+const fcis = require('./fcis')
+
+class Tracker {
+  constructor(telegramBot) {
+    this.telegramBot = telegramBot;
+  }
+  initialize = async () => {
+    this.setDataAndNotify();
+
+    const rule = new schedule.RecurrenceRule();
+    rule.dayOfWeek= new schedule.Range(1, 5);
+    rule.hour = 21;
+    rule.minute = 5;
+    rule.tz = "America/Argentina/Buenos_Aires";
+
+    schedule.scheduleJob(rule, () => {
+      this.setDataAndNotify();
+      
+    })
+  }
+
+  setDataAndNotify = async () => {
+    const fciDataList = await this.getDataFromFCIS(fcis)
+
+    this.telegramBot.telegram.sendMessage(
+      process.env.CHAT_ID,
+      fciDataList.join("\n")
+    )
+    console.info("update sent!", new Date().toLocaleTimeString("es-AR"));
+  }
+
+  getDataFromFCI = async (fund, fundClass) => {
+    try {
+      const result = await axios.get(
+        `https://api.cafci.org.ar/fondo/${fund}/clase/${fundClass}/ficha`
+      );
+
+      return result.data.data.info;
+    } catch (err) {
+      throw new Error("Error in data fetch fund");
+    }
+  }
+
+  getDataFromFCIS = async fundList => {
+    const fciDataList = new Array();
+    try {
+      await Promise.all(fundList.map(async (fci) => {
+        const fciData = await this.getDataFromFCI(fci.fund, fci.fundClass);
+        fciDataList.push(this.getMessageForMsg(fci.name, fciData));
+      }))
+    } catch (err) {
+      throw new Error("Error in data fetch list of funds")
+    }
+    return fciDataList;
+  }
+
+  getMessageForMsg = (fciName, fciData) => {
+    return `FCI ${fciName} - ${fciData.diaria.rendimientos.day.fecha} - diario: ${fciData.diaria.rendimientos.day.rendimiento} - mensual ${fciData.diaria.rendimientos.month.rendimiento}`
+  }
+}
+
+const telegramBot = new Telegraf(process.env.ACCESS_TOKEN_TELEGRAM);
+const tracker = new Tracker(telegramBot);
+tracker.initialize()
